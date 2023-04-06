510ca1b73b0bbdcd4d31f30382537f66a9c634f0
Murilo-Sanches <sanchesmurilo199@outlook.com>
2023-04-06 11:32:35 -0300

    Dictionarys

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Sharp.Extensions;
 
 namespace Sharp
@@ -14,6 +15,29 @@ namespace Sharp
 
             Console.WriteLine(number.IsEven());
             Console.WriteLine((50).IsEven());
+
+            Dictionary<String, string> dict = new Dictionary<string, string>();
+            dict.Add("key", "value");
+            try
+            {
+                dict.Add("key", "value");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("An element with Key = \"key\" already exists.");
+            }
+
+            dict["key"] = "Outro Value";
+
+            dict["key2"] = "Criando";
+
+            string value = "";
+            Console.WriteLine($"{(dict.TryGetValue("nonexistent", out value) ? "Existe" : "Inexistente")}");
+
+            foreach (KeyValuePair<string, string> kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
         }
     }
 }
